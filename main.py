a11bd404152404f287e8605763f2b1321f59ae86
Kevyn gallego <gallegokevyn10@gmail.com>
2024-02-05 16:31:23 -0500

    reto dia #1

---
+++ main.py
@@ -0,0 +1,9 @@
+#Registro de un usuario
+
+full_name = input('ingresa tu nombre ')
+last_name = input('ingresa tus apellidos ')
+phone=input('ingresa tu telefono')
+email= input('Ingresa tu Correo')
+
+print(' Hola ' + full_name + ' ' + last_name + ' ' +  'en breve recibiras un correo a '+ email)
+
